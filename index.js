80cd3eaa3ef49c7ab05a5770dab216f7c268dff3
Rickardin Richard <r.rickardin@outlook.com>
2025-06-21 20:31:13 -0400

    Completed

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+const date = require('datejs');
 
+function combineUsers(...args) {
+  // Initialize return object
+  const combinedObject = {
+    users: []
+  };
+
+  // Loop through args and merge arrays
+  for (const array of args) {
+    combinedObject.users = [...combinedObject.users, ...array];
+  }
+
+  // Add today's date
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
 
 
 module.exports = {
